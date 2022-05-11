67ee0bc8ef122a4330217bf90f60a27a628ba156
stark920 <hautang920@gmail.com>
2022-05-11 17:42:34 +0800

    f

--- src/views/SignView.vue
+++ src/views/SignView.vue
@@ -16,7 +16,7 @@ if (user.name) {
   </div>
   <main class="flex h-screen w-screen items-center justify-center">
     <div
-      class="custom-border dark:dark-card flex w-11/12 max-w-[900px] items-center bg-white px-12 py-16"
+      class="custom-border dark:dark-card flex w-11/12 max-w-[900px] items-center bg-white px-6 py-8 md:px-12 md:py-16"
     >
       <div class="hidden md:block">
         <img src="@/assets/login_page.svg" alt="login_banner" />
